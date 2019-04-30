c1a721bf883598438ed92f5e17d8a6448d960e9e
Marc Thiongo <marcthuo@gmail.com>
2019-04-30 16:46:13 -0400

    index edit

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 7000;
 server.listen(port, () =>
-console.log(`\n*** Server Running on http://localhost:7000 ***\n`),
+console.log(`\n*** Server Running on http://localhost:${port} ***\n`),
 );
