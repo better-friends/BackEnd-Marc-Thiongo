2602feb177a46b562cf9f05cdc4660d59f85d329
Marc Thiongo <marcthuo@gmail.com>
2019-04-30 20:02:58 -0400

    index and .env/package json edits

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 7000
+const port = process.env.PORT || 3000
 
 server.listen(port, () => {
 console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
